d8baf662f5242e4175458f2791cedd04776a6180
BustamanteMathias <48842768+BustamanteMathias@users.noreply.github.com>
2019-03-26 20:19:00 -0300

    Add files via upload

---
+++ Clase_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria;
+
+namespace Clase_03
+{
+    class Clase_03
+    {
+        static void Main(string[] args)
+        {
+            // USO DE OBJETO CARGADO POR REFERENCIA .dll DESDE EL USO DE using Libreria
+
+            Libreria.Test.Saludar();
+            Console.ReadKey();
+        }
+    }
+}

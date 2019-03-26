517723af9c5031fd8eb3974ed46040f9d9e4e0bc
BustamanteMathias <48842768+BustamanteMathias@users.noreply.github.com>
2019-03-26 20:25:06 -0300

    Add files via upload

---
+++ Libreria/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Libreria
+{
+    public class Test
+    {
+        public static string Saludar()
+        {
+            string saludo = "HOLA MUNDO, DESDE LIBRERIA";
+
+            return saludo;
+        }
+    }
+}
